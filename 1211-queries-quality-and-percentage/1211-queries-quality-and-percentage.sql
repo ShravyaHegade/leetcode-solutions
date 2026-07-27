616cbe1dc9e225e85f3184dffae16ab3b5b1bdcc
Shravya Ravindra Hegade <hegdeshrav22@gmail.com>
2026-07-27 19:17:47 +0530

    Time: 605 ms (5.18%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,19 @@
+SELECT
+    query_name,
+    ROUND(SUM(rating / position) / COUNT(rating), 2) AS quality,
+    ROUND(
+        (COUNT(
+            CASE
+                WHEN rating < 3 THEN 1
+                ELSE NULL
+            END
+        ) * 100.0) / COUNT(rating),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
+
+
+
+
+

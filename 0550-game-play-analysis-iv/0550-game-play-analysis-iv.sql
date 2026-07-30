215d52d362d0f3d0f9da9666583ce027ee7777a8
Shravya Ravindra Hegade <hegdeshrav22@gmail.com>
2026-07-30 06:41:13 +0530

    Time: 571 ms (62.96%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,21 @@
+SELECT
+    ROUND(
+        COUNT(*) * 1.0 / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM
+(
+    SELECT
+        a.player_id
+    FROM Activity a
+    JOIN
+    (
+        SELECT
+            player_id,
+            MIN(event_date) AS first_login
+        FROM Activity
+        GROUP BY player_id
+    ) f
+    ON a.player_id = f.player_id
+    WHERE a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY)
+) t;

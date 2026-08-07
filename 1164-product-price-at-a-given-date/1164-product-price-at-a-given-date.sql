d7e99fd9d191370c0503d58b3b11d7084da7bf97
Shravya Ravindra Hegade <hegdeshrav22@gmail.com>
2026-08-07 07:53:15 +0530

    Time: 509 ms (55.25%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,21 @@
+SELECT product_id, new_price AS price
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY product_id
+               ORDER BY change_date DESC
+           ) AS row_num
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) t
+WHERE row_num = 1
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+);

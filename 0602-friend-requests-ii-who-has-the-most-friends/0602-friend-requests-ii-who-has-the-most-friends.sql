aac3a92b039e8b1166f9723ea1af33e452345572
Shravya Ravindra Hegade <hegdeshrav22@gmail.com>
2026-08-13 06:59:55 +0530

    Time: 364 ms (27.62%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,17 @@
+SELECT id, SUM(num) AS num
+FROM (
+    SELECT requester_id AS id,
+           COUNT(accepter_id) AS num
+    FROM RequestAccepted
+    GROUP BY requester_id
+
+    UNION ALL
+
+    SELECT accepter_id AS id,
+           COUNT(requester_id) AS num
+    FROM RequestAccepted
+    GROUP BY accepter_id
+) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
